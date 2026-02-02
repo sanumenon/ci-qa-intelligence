ef0084ffb1af4fd047bc3c9e15d84002fd3b90e4
Sanu Menon <sanumenon@users.noreply.github.com>
2026-02-02 15:04:14 +0530

    Add domain validation function and allowed domains

--- worker/domainGuard.js
+++ worker/domainGuard.js
@@ -1,1 +1,9 @@
+const ALLOWED_DOMAINS = [
+  "charitableimpact.com",
+  "my.charitableimpact.com"
+];
+
+export function validateDomain(url) {
+  return ALLOWED_DOMAINS.some(d => url.includes(d));
+}
 
